ab2495e6c46a93ff833deb99981d6a9a5c29ff30
Andrei Cioara <andrei@cioara.me>
2018-05-22 14:02:20 +0100

    More comments

--- examples/example4.jsx
+++ examples/example4.jsx
@@ -67,6 +67,7 @@ class Example3 extends Component {
             onSelect: () => {},
             statusComponent: () => <span></span>,
             limit: termSize().rows - 2, // One for clear screen, one for cursor
+                                        // Could be 1 more for statusComponent if that exists
         };
 
         return <span>
